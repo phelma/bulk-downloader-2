2e89187940e3f0168d88147586f92b83930c6add
Phil Helm <philip.helm@omgplc.com>
2016-12-09 12:46:11 +0000

    less error logging

--- wget.js
+++ wget.js
@@ -3,11 +3,7 @@
 let exec = require('child_process').execFile;
 
 module.exports = (url, location, callback) => {
-  // wget --quiet --continue --output-document=file
   exec('wget', ['--quiet', '--continue', `--output-document=${location}`, url], (err, stdout, stderr) => {
-    if (err || stderr){
-      console.log('ERROR', err, stderr);
-    }
     callback && callback(err, stdout);
   })
 }
