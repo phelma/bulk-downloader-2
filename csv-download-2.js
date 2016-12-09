85bd188c54639980cb8c977198c26b9b1d7ad43a
Phil Helm <philip.helm@omgplc.com>
2016-12-09 12:51:08 +0000

    add wget csv downloader

---
+++ csv-download-2.js
@@ -0,0 +1,81 @@
+'use strict';
+
+Error.stackTraceLimit = Infinity;
+
+let parallel      = 10;
+let inFile        = '18k_download.csv';
+let inFiles       = [inFile];
+let outFile       = __dirname + '/download/';
+
+let path          = require('path');
+let fs            = require('fs');
+
+let parse         = require('csv-parse');
+let async         = require('async');
+let mkdirp        = require('mkdirp');
+
+let wget = require('./wget');
+
+console.log('lets go');
+console.time('downloaded all');
+
+let errors = [];
+
+let getTheFiles = (file, callback) => {
+  console.log('STARTING ' + file);
+  fs.readFile(inFile, 'utf8', (err, res) => {
+
+    parse(res, (err, out) => {
+      let start = Date.now();
+      let fileList = out;
+      let count = 0;
+      let total = fileList.length;
+
+      console.log(`Downloading ${total} files`);
+
+      async.eachLimit(fileList, parallel, (file, done) => {
+        let finished = false;
+
+        let dlFileName = file[1].split('\\').join('/');
+        let url = file[0];
+        let outFilename = outFile + dlFileName;
+        let outDirname = path.dirname(outFilename);
+        mkdirp(outDirname, () => {
+          // console.log(`${n} / ${total} - Downloading`);
+          // console.time(`${n} / ${total} - DONE`);
+
+          wget(url, outFilename, (err) => {
+              if (err){
+                errors.push(err);
+              } else {
+                let now = Date.now();
+                let diff = now - start;
+                let n = ++ count;
+                if (n % 10 === 0){
+                  let avg = diff / count;
+                  console.log(`Done: ${n}, Failed: ${errors.length}`);
+                  console.log('AVERAGE TIME', avg);
+                }
+              }
+              done();
+
+          })
+
+        })
+
+      }, (err) => {
+        console.timeEnd('FINISHED ' + file);
+        console.log('ERRORS: ' + errors.length);
+      })
+
+    })
+
+  });
+}
+
+async.eachSeries(inFiles, (file, done) => {
+  getTheFiles(file, done);
+}, (err) => {
+  console.timeEnd('downloaded all');
+  console.log('DONE THEM ALL');
+});
